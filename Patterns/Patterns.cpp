8c6db446f31ca08ab3d6ed4382e64067164da42a
Piyush Suthar <Piyushsuthar524@gmail.com>
2024-09-14 02:21:34 +0530

    Update Patterns.cpp

--- Patterns/Patterns.cpp
+++ Patterns/Patterns.cpp
@@ -3,51 +3,51 @@
 // #include<iostream>
 // using namespace std;
 
-// int main(){
-//    int i , j;
+int main(){
+   int i , j;
 
-//    for(int i = 0; i < 4; i++){
-//     for(int j =  0;  j < 4; j++){
-//       cout << "*";
-//     }
-//     cout << endl;
-//    }
-//    return 0;
-// }
+   for(int i = 0; i < 4; i++){
+    for(int j =  0;  j < 4; j++){
+      cout << "*";
+    }
+    cout << endl;
+   }
+   return 0;
+}
 
 // function method 
-// #include<iostream>
-// using namespace std;
-// void pattern1( int n){
-//   for(int i = 0; i < n; i++ ){
-//     for(int j = 0; j < n; j++){
-//       cout << "*";
-//     }
-//     cout << endl;
-//   }
-// }
-// int main(){
-//   int n;
-//   cin >> n;
-//   pattern1( n);
-// }
+#include<iostream>
+using namespace std;
+void pattern1( int n){
+  for(int i = 0; i < n; i++ ){
+    for(int j = 0; j < n; j++){
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+int main(){
+  int n;
+  cin >> n;
+  pattern1( n);
+}
 
 // down- triangle pattern 
-// #include<iostream>
-// using namespace std;
-// void pattern2(int n){
-//   for(int i = 0; i < n; i++){
-//     for(int j = 0; j <= i; j++){
-//       cout << "*";
-//     }
-//     cout << endl;
-//   }
-// }
-// int main(){
-//   int n;
-//   cin >> n;
-//   pattern2(n);
-// }
+#include<iostream>
+using namespace std;
+void pattern2(int n){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j <= i; j++){
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+int main(){
+  int n;
+  cin >> n;
+  pattern2(n);
+}
 
 
 //number-down triangle pattern
@@ -68,3 +68,44 @@ int main(){
   cin >> n;
   pattern3(n);
 }
+
+
+//equal number down triangle
+#include<iostream>
+using namespace std;
+
+void pattern4(int n){
+  for(int i = 1; i <= n; i++){
+    for(int j = 1; j <=i; j++){
+      cout << i << " ";
+    }
+    cout << endl;
+  }
+}
+int main(){
+  int n;
+   cin >> n;
+   pattern4(n);
+}
+
+//reverse triangle star
+
+#include<iostream>
+using namespace std;
+
+void pattern5(int n){
+  for(int i = 1; i <= n; i++){
+    for(int j = 0; j < n-i +1; j++){
+      cout << "*";
+    }
+
+    cout << endl;
+  }
+}
+
+int main(){
+  int n;
+  cin >> n;
+  pattern5(n);
+}
+
