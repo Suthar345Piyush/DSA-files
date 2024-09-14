086985c5697825a2c95901a7c1db620610637621
Piyush Suthar <Piyushsuthar524@gmail.com>
2024-09-14 14:58:53 +0530

    Update Patterns.cpp

--- Patterns/Patterns.cpp
+++ Patterns/Patterns.cpp
@@ -1,7 +1,7 @@
 //Patterns in cpp
 // 4star in row-coloumn
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
 int main(){
    int i , j;
@@ -108,4 +108,84 @@ int main(){
   cin >> n;
   pattern5(n);
 }
+
+//reverse triangle number:
+#include<iostream>
+using namespace std;
+
+void pattern6(int n){
+  for(int i = 0; i <= n; i++){
+    for(int j = 1; j < n-i+1; j++){
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main(){
+  int n;
+  cin >> n;
+  pattern6(n);
+  }
+
+//star pyramid
+
+#include<iostream>
+using namespace std;
+void pattern7(int n){
+
+  for(int i = 0; i <= n; i++){
+    //space formula = n -i -1
+    for(int j = 0; j < n-i-1; j++){
+    cout << " ";
+    }
+  
+    //star formula = 2*i+1
+   for(int j = 0; j < 2*i+1; j++){
+    cout << "*";
+   }
+  
+  //space  formula = n - i -1
+  for(int j = 0; j < n-i-1; j++){
+    cout << " ";
+       }
+       cout << endl;
+     }
+   }
+
+int main(){
+  int n;
+    cin >> n;
+    pattern7(n);
+}
+
+// reverse star pyramid
+
+
+#include<iostream>
+using namespace std;
+void pattern8(int n){
+  for(int i = 0; i < n; i++){
+    //space
+    for(int j = 0; j < i; j++){
+      cout << " ";
+    }
+    //star formula  = 2*n - (2*i+1)
+    for(int j =0; j< 2*n - (2*i+1); j++){
+     cout << "*";
+    }
+    //space 
+    for(int j = 0; j < i; j++){
+      cout << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main(){
+  int n;
+  cin >> n;
+  pattern8(n);
+}
+
 
