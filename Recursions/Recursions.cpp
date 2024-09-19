a0381930388113a9ebb68acd2d6538c1d55751ca
Piyush Suthar <Piyushsuthar524@gmail.com>
2024-09-19 22:48:37 +0530

    Update Recursions.cpp

--- Recursions/Recursions.cpp
+++ Recursions/Recursions.cpp
@@ -1,16 +1,32 @@
 //infinite recursion example
 //function calling itself again and again
 
-// #include<bits/stdc++.h>
-// using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
 
-// void fn(){
-//   cout << "Hello" << endl;
-//   fn();
-// }
-// int main(){
-//   fn();
-// }
+void fn(){
+  cout << "Hello" << endl;
+  fn();
+}
+int main(){
+  fn();
+}
 
 
 //Base condition is to stop the recursion
+
+#include<bits/stdc++.h>
+using namespace std;
+int cnt = 0;
+
+void fn(){
+  if(cnt == 4) return;
+  cout << cnt << endl;
+  cnt++;
+
+  fn();
+}
+
+int main(){
+  fn();
+}
