cd13f640bc8562b4284f52918ac93abf18b904bb
Piyush Suthar <Piyushsuthar524@gmail.com>
2024-09-19 22:39:33 +0530

    Add files via upload

---
+++ Recursions/Recursions.cpp
@@ -0,0 +1,16 @@
+//infinite recursion example
+//function calling itself again and again
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void fn(){
+//   cout << "Hello" << endl;
+//   fn();
+// }
+// int main(){
+//   fn();
+// }
+
+
+//Base condition is to stop the recursion
