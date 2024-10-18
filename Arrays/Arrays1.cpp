f3e1c7ced87fd10f3f2416181b858d459dcb7dc9
Piyush Suthar <Piyushsuthar524@gmail.com>
2024-10-19 00:33:10 +0530

    Add files via upload

---
+++ Arrays/Arrays1.cpp
@@ -0,0 +1,18 @@
+// find the largest number in the given array
+// #include<bits/stdc++.h>
+// using namespace std;
+
+
+// int main(){
+//   int n;
+//   int arr[n] = {3, 2, 1, 5, 2};
+//   int largest = arr[0];
+//   for(int i=0; i<n; i++){
+//     if(arr[i] > largest){
+//       largest = arr[i];
+//     }
+//    cout << "The largest element in the array is: " << largest << endl;
+//   }
+  
+// }
+//
